ebfc438b1a018a33095a05d56af4acc0724d30ec
Ansel Rosenberg <ansel01@gmail.com>
2015-03-02 01:32:53 -0800

    Add HTTP Collect

--- http-get.js
+++ http-get.js
@@ -1,9 +1,16 @@
 var http = require('http');
 
 http.get(process.argv[2], function(res) {
+	var data = '';
 	res.setEncoding('utf8');
-	res.on('data', function(data) {
+
+	res.on('data', function(chunk) {
+    data += chunk;
+  });
+  res.on('end', function() {
+    console.log(data.length);
     console.log(data);
   });
+
 });
 
